e2c53737f2c8678702ece461bd081fc122070241
Tushar <102414173+tushar7158@users.noreply.github.com>
2024-09-10 12:07:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,2 @@
-hosts: devops
-  user: ansible
-  become: yes
-  connection: ssh
-  tasks :
-    - name: installing server
-      action: yum name=httpd state=present 
-
-https://packages.chef.io/files/stable/chef-workstation/0.4.2/el/7/chef-workstation-0.4.2-1.el6.x86_64.rpm
+a=5
+print(a)
